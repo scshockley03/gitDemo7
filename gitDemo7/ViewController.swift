e8b2ee39c0bc1b8723df29590c034d7e92e059bb
Stanley Shockley <scshockley03@yahoo.com>
2022-02-26 07:33:22 -0800

    Initial Commit

---
+++ gitDemo7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitDemo7
+//
+//  Created by Stan Shockley on 2/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
